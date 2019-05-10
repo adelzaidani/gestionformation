e265009f2f66b2fa4e1131b17c68d205ca515600
adel <adelzaidani@gmail.com>
2019-05-10 02:12:22 +0200

    essai transformation facture version html vers la version pdf

--- site_formation/billing/urls.py
+++ site_formation/billing/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from .views import print_invoice
 
 
+
 app_name='billing'
 
 urlpatterns = [
     path('<int:id_session>/',print_invoice,name='print_invoice'),
+
     ]
--- site_formation/billing/views.py
+++ site_formation/billing/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,get_object_or_404
-from decimal import Decimal
+from django.http import HttpResponse
+from django.views.generic import View
+from .utils import render_to_pdf
 
 from .models import Invoice
 
@@ -7,8 +9,18 @@ from .models import Invoice
 verifier avant d'afficher la facture que le client dispose bien d'une facture 
 pour le numero de session passer en parametre
 '''
+
 def print_invoice(request,id_session ):
     client=request.user.profile
     invoice=get_object_or_404(Invoice.objects.filter(session=id_session,client=client))
 
     return render(request,'billing/invoice.html',{'invoice':invoice})
+
+'''
+def print_invoice(request,id_session ):
+    client=request.user.profile
+    invoice=get_object_or_404(Invoice.objects.filter(session=id_session,client=client))
+
+    return render_to_pdf('billing/invoice.html',{'invoice':invoice})
+'''
+
