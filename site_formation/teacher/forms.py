df6ff365425a081e70bb02b4f82d2be81478a955
adel <adelzaidani@gmail.com>
2019-05-15 04:11:04 +0200

    Recherche d'une solution pour l'enregistrement des pésences des eleves, je me dirige vers jquery.

--- site_formation/teacher/forms.py
+++
@@ -1,9 +0,0 @@
-from .models import Attendance
-from django.forms import ModelForm
-
-class AttendanceForm(ModelForm):
-    class Meta:
-        model=Attendance
-        fields=['student','session','date_of_attendance', 'attendance_choices']
-
-
--- site_formation/teacher/urls.py
+++ site_formation/teacher/urls.py
@@ -6,7 +6,7 @@ app_name='teacher'
 urlpatterns =[
     path('my_courses_teacher/', views.session_teacher, name='courses_teacher'),
     path('training_student/<int:id_session>',views.training_student,name='training_student'),
-    path('list_attendance/',views.list_attendance, name='list_attendance'),
+    path('list_attendance/<int:id_session>',views.list_attendance, name='list_attendance'),
 
 
 ]
