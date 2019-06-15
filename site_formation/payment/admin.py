96309b956af6aa0b7f99dd813d2e58f5da2643b2
adel <adelzaidani@gmail.com>
2019-06-15 02:17:04 +0200

    avancée dans la partie admin /paiement

--- site_formation/payment/admin.py
+++ site_formation/payment/admin.py
@@ -13,6 +13,7 @@ class PaymentSummaryAdmin(admin.ModelAdmin):
     change_list_template = 'admin/payment_summary_change_list.html'
     date_hierarchy = 'date_payment'
 
+
     def changelist_view(self, request, extra_context=None):
         response = super().changelist_view(
             request,
@@ -30,58 +31,16 @@ class PaymentSummaryAdmin(admin.ModelAdmin):
         }
 
         response.context_data['summary'] = list(
-            qs
-                .values('session')
-                .annotate(**metrics)
-                .order_by('-total_sales')
+            qs.values('session__training__name').annotate(**metrics).order_by('-total_sales')
         )
+
         response.context_data['summary_total'] = dict(
             qs.aggregate(**metrics)
         )
 
-        period = get_next_in_date_hierarchy(
-            request, self.date_hierarchy)
-
-        response.context_data['period'] = period
-
-        summary_over_time = qs.annotate(
-            period=Trunc(
-                'date_payment',
-                period,
-                output_field=DateTimeField(),
-            ),
-        ).values('period') \
-            .annotate(total=Sum('amount')) \
-            .order_by('period')
-
-        summary_range = summary_over_time.aggregate(
-            low=Min('total'),
-            high=Max('total'),
-        )
-        high = summary_range.get('high', 0)
-        low = summary_range.get('low', 0)
-
-        response.context_data['summary_over_time'] = [{
-            'period': x['period'],
-            'total': x['total'] or 0,
-            'pct': \
-                ((x['total'] or 0) - low) / (high - low) * 100
-                if high > low else 0,
-        } for x in summary_over_time]
 
         return response
 
-def get_next_in_date_hierarchy(request, date_hierarchy):
-    if date_hierarchy + '__day' in request.GET:
-        return 'hour'
-
-    if date_hierarchy + '__month' in request.GET:
-        return 'day'
-
-    if date_hierarchy + '__year' in request.GET:
-        return 'week'
-
-    return 'month'
 
 
 
